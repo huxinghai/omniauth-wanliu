9a1eb27f3c63cdc54a52eb274dbe9207cf8641d9
kaka <huxinghai1988@gmail.com>
2013-01-17 15:10:54 +0800

    add rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+require 'rubygems'
+require "rspec"
+require 'active_support/all' 
+
+require File.expend_path("../../lib/omniauth-wanliu", __FILE__)
---
+++ spec/wanliu_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+
+describe OmniAuth::Strategies::WanliuId do 
+    subject do 
+        puts OmniAuth::Strategies::WanliuId.default_options.client_options 
+    end
+
+    context "test omniauth wanliu options" do 
+
+        it "should have correct site name" do 
+        end
+        
+    end
+end
