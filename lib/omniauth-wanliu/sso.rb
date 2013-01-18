5ac4e64b22542e58fe2742adeea6c7b6714d79de
kaka <huxinghai1988@gmail.com>
2013-01-18 15:00:24 +0800

    fixed config

--- lib/omniauth-wanliu/sso.rb
+++ lib/omniauth-wanliu/sso.rb
@@ -1,8 +1,14 @@
 module OmniAuth
     module Wanliu
         def self.config
-            path = File.expand_path("../../../config/sso.yaml", __FILE__)            
-            File.file?(path) ?  YAML::load_file(path)["accounts"] : raise("not exists config/sso.yml file ?")
+            path = test_mode ?  "../../../spec/config/sso.yml" : "../../../config/sso.yml"
+            full_path = File.expand_path(path, __FILE__)            
+            File.file?(full_path) ?  YAML::load_file(full_path)["accounts"] : raise("not exists #{full_path} file ?")
+        end
+
+        def self.test_mode(state = nil)
+            @test_model = state unless state.nil?
+            @test_model
         end
     end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,8 @@ require "rspec"
 require "http"
 require "application_controller"
 
+require File.expand_path("../../lib/omniauth-wanliu/sso", __FILE__)
+OmniAuth::Wanliu.test_mode(true)
 require File.expand_path("../../lib/omniauth-wanliu", __FILE__)
+
 require 'ajax_auth_controller'
